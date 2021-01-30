a16d1891f9bdc698bed9955a32131ed0750ac175
very jazzy <jackwilliamcastle@outlook.com>
2021-01-30 10:12:45 +0000

    delete button functionality added!

--- src/main/java/journalProject/JournalApplication.java
+++ src/main/java/journalProject/JournalApplication.java
@@ -1,7 +1,5 @@
 package journalProject;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import org.apache.tomcat.util.json.JSONParser;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
@@ -9,21 +7,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Locale;
+import java.util.Iterator;
 import java.util.UUID;
 
 @SpringBootApplication
@@ -43,6 +37,56 @@ public class JournalApplication {
         return "redirect:/entries";
     }
 
+    @GetMapping("/deleteEntry/{id}")
+    public String deleteEntry(@PathVariable("id") String id) {
+        try {
+            JSONObject obj = new JSONObject(Files.readString(Path.of("src/main/resources/Log.json")));
+            JSONArray entries = (JSONArray) obj.get("entries");
+            entries.remove(findIdIndex(entries, id));
+
+            FileWriter myWriter = new FileWriter("src/main/resources/Log.json", false);
+            myWriter.write(obj.toString());
+            myWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/entries";
+    }
+
+    public int findIdIndex(JSONArray entries, String id) {
+        for (int i = 0; i < entries.length(); i++) {
+            JSONObject entry = (JSONObject) entries.get(i);
+            if (entry.get("id").equals(id)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    @GetMapping("/entries")
+    public String greeting(Model model) throws IOException {
+        ArrayList<Entry> entryList = new ArrayList<>();
+
+        JSONObject jsonObject = new JSONObject(Files.readString(Path.of("src/main/resources/Log.json")));
+        JSONArray jsonArray = (JSONArray) jsonObject.get("entries");
+
+        for (Object obj : jsonArray) {
+            JSONObject aJsonObject = (JSONObject) obj;
+            String id = (String) aJsonObject.get("id");
+            String date = (String) aJsonObject.get("date");
+            String title = (String) aJsonObject.get("title");
+            String message = (String) aJsonObject.get("message");
+            Entry entry = new Entry(id, date, title, message);
+            entryList.add(entry);
+        }
+
+        model.addAttribute("entries", entryList);
+        return "entries";
+    }
+
+
+
     private JSONObject writeToJSONObj(String date, String title, String message) {
         JSONObject obj = new JSONObject();
         obj.put("id", getIDNumber());
@@ -51,6 +95,7 @@ public class JournalApplication {
         obj.put("message", message);
         return obj;
     }
+
     private String getIDNumber() {
         return UUID.randomUUID().toString();
     }
@@ -77,26 +122,7 @@ public class JournalApplication {
         return formattedDate.toString();
     }
 
-    @GetMapping("/entries")
-    public String greeting(Model model) throws IOException {
-        ArrayList<Entry> entryList = new ArrayList<>();
 
-        JSONObject jsonObject = new JSONObject(Files.readString(Path.of("src/main/resources/Log.json")));
-        JSONArray jsonArray = (JSONArray) jsonObject.get("entries");
-
-        for (Object obj : jsonArray) {
-            JSONObject aJsonObject = (JSONObject) obj;
-            String id = (String) aJsonObject.get("id");
-            String date = (String) aJsonObject.get("date");
-            String title = (String) aJsonObject.get("title");
-            String message = (String) aJsonObject.get("message");
-            Entry entry = new Entry(id, date, title, message);
-            entryList.add(entry);
-        }
-
-        model.addAttribute("entries", entryList);
-        return "entries";
-    }
 
 
 }
