4dded50f8409e99e6e2ce81486958ef80b4a4ca9
very jazzy <jackwilliamcastle@outlook.com>
2021-02-01 11:58:44 +0000

    controller class refactor

--- src/main/java/journalProject/JournalApplication.java
+++ src/main/java/journalProject/JournalApplication.java
@@ -2,57 +2,13 @@ package journalProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.UUID;
 
 @SpringBootApplication
 
-@Controller
 public class JournalApplication {
-
-    final JsonDao database = new JsonDao();
 
     public static void main(String[] args) {
         SpringApplication.run(JournalApplication.class, args);
     }
-
-    @PostMapping("/sendForm")
-    public String handleForm(@RequestParam(name = "user_date") String date, @RequestParam(name = "user_title") String title, @RequestParam(name = "user_message") String message) {
-        date = formatDate(date);
-        String id = UUID.randomUUID().toString();
-
-        Entry entry = new Entry(id, date, title, message);
-        database.add(entry);
-        return "redirect:/entries";
-    }
-
-    @GetMapping("/deleteEntry/{id}")
-    public String deleteEntry(@PathVariable("id") String id) {
-        database.delete(id);
-        return "redirect:/entries";
-    }
-
-    @GetMapping("/entries")
-    public String greeting(Model model) throws IOException {
-        ArrayList<Entry> entryList = database.getEntries();
-        model.addAttribute("entries", entryList);
-        return "entries";
-    }
-
-    private String formatDate(String date) {
-        String formattedDate = date.substring(8) + "/" + date.substring(5, 7) + "/" + date.substring(0, 4);
-        return formattedDate.toString();
-    }
-
-
-
 
 }
---
+++ src/main/java/journalProject/MySqlDao.java
@@ -0,0 +1,21 @@
+package journalProject;
+
+import java.util.ArrayList;
+
+public class MySqlDao implements Dao{
+
+    @Override
+    public void add(Object entry) {
+
+    }
+
+    @Override
+    public void delete(String id) {
+
+    }
+
+    @Override
+    public ArrayList<Entry> getEntries() {
+        return null;
+    }
+}
